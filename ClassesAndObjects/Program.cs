dbbe18b9072b9a6332a5ed758b3f310373953a0e
andres3204 <101659149+andres3204@users.noreply.github.com>
2022-06-02 18:49:46 -0500

    Update Program.cs

--- ClassesAndObjects/Program.cs
+++ ClassesAndObjects/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using System.Text;
 
@@ -8,21 +8,23 @@ namespace ClassesAndObjects
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book();
-            Book book2 = new Book();
+         /*Parameters*/   Book book1 = new Book("Harry Potter", "JK Rowling", 280);
+         /*Parameters*/   Book book2 = new Book("Bible", "God", 1250);
+                          Book book3 = new Book();
             // OBJECTS
-            book1.title = "Harry Potter";
-            book1.name = "JK Rowling";
-            book1.pages = 280;
+            /* book1.title = "Harry Potter";
+             book1.name = "JK Rowling";
+             book1.pages = 280;
 
-            book2.title = "Bible";
-            book2.name = "God";
-            book2.pages = 1250;
+             book2.title = "Bible";
+             book2.name = "God";
+             book2.pages = 1250;
 
             Console.WriteLine(book1.title);
             Console.WriteLine(book2.title);
-
+            */
             Console.ReadLine();
+
         }
     }
 }
