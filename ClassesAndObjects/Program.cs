2852dfa104347054b491e19945d99811ded7a647
Andres Enrique Jiménez Mogollón <andresejimenez@hotmail.com>
2022-06-02 18:12:02 -0500

    Add project files.

---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            Book book2 = new Book();
+            // OBJECTS
+            book1.title = "Harry Potter";
+            book1.name = "JK Rowling";
+            book1.pages = 280;
+
+            book2.title = "Bible";
+            book2.name = "God";
+            book2.pages = 1250;
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book2.title);
+
+            Console.ReadLine();
+        }
+    }
+}
